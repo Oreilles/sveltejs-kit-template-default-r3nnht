6d36c5eede5340867c37a573f41bb9c9afde046e
Oreilles <33065839+Oreilles@users.noreply.github.com>
2023-01-19 14:11:40 +0100

    5 files added, 5 deleted

--- src/routes/[[locale=locale]]/+layout.svelte
+++ src/routes/[[locale=locale]]/+layout.svelte
@@ -3,5 +3,7 @@
 </script>
 
 <h1> In locale: {$page.params.locale} </h1>
+<h1> In category: {$page.params.category} </h1>
+<h1> In page: {$page.params.slug} </h1>
 
 <slot/>
--- src/routes/[[locale=locale]]/[category]/[...slug]/+page.svelte
+++ src/routes/[[locale=locale]]/[category]/[...slug]/+page.svelte
@@ -1,4 +1,4 @@
 
 <h2><a href="/foo/bar/baz">/foo/bar/baz</a></h2>
-<h2><a href="/fr/foo/bar/baz">/en/foo/bar/baz</a></h2>
+<h2><a href="/fr/foo/bar/baz">/fr/foo/bar/baz</a></h2>
 <h2><a href="/en/foo/bar/baz">/en/foo/bar/baz</a></h2>
